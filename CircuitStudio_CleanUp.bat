1d0876d14f213a945216602695b8e8c6dc461a2c
x-io Technologies <social@x-io.co.uk>
2019-02-22 15:44:17 +0000

    Generate project outputs

--- CircuitStudio_CleanUp.bat
+++ CircuitStudio_CleanUp.bat
@@ -14,10 +14,6 @@ echo Removing "Logs" directories recursively...
 for /f "delims==" %%i in ('dir /b /on /s "%~p0*Logs"') do if "%%~ni"=="Logs" rd "%%i" /s /q
 echo.
 
-echo Removing "Outputs" directories recursively...
-for /f "delims==" %%i in ('dir /b /on /s "%~p0*Outputs"') do if "%%~ni"=="Outputs" rd "%%i" /s /q
-echo.
-
 echo Removing "Project Logs for " directories recursively...
 for /f "delims==" %%i in ('dir /b /on /s "%~p0*Project Logs for *"') do rd "%%i" /s /q
 echo.
